cac17a40ba81bef2e1246a19498eea36395f2c4b
Pablo Vasconcelos <pablo.vasconcelos.cefet@gmail.com>
2024-02-17 19:20:56 -0300

    Adicionado folha de estilo e personalização dos campos

--- node/serverModule.js
+++ node/serverModule.js
@@ -7,33 +7,35 @@ const cors = require('cors');
 const templateFiles = path.join(__dirname, '../template')
 const imgFiles = path.join(__dirname, '../src/img')
 const fontFiles = path.join(__dirname, '../fonts/')
+const styleFiles = path.join(__dirname, '../styles/')
 
 const defaultPort = 8088
 
 
 const app = express()
 app.use(cors());
+app.use(express.static(styleFiles))
 app.use(express.static(templateFiles))
 app.use(express.static(imgFiles))
 app.use(express.static(fontFiles))
 
 //Permissões para permitir a comunicação entre portas
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 
 
 app.get('', (req, res, next) => {
-    res.sendFile(path.join(templateFiles, '/index.html'))
+  res.sendFile(path.join(templateFiles, '/index.html'))
 })
 
 
 const server = http.createServer(app) //Abrir o servidor HTTP
 server.listen(defaultPort, () => { //Servidor aberto e ouvindo as portas para requisições
-    console.log(`Servidor HLS rodando em http://localhost:${defaultPort}`)
-  })
+  console.log(`Servidor HLS rodando em http://localhost:${defaultPort}`)
+})
